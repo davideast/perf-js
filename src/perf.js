1c6c206d3532ad8bd64c90b62f548d545931b2aa
David <david@firebase.com>
2014-12-06 16:50:32 -0800

    Move helper functions into perf

--- src/perf.js
+++ src/perf.js
@@ -1,142 +1,6 @@
 (function(window) {
   "use strict";
 
-  // create immutable listener object
-  function listener(params) {
-    return Object.freeze({
-      name: params.name,
-      event: params.event
-    });
-  }
-
-  // find all items in a collection that match a comparison's criteria
-  function findWhere(collection, compare) {
-    var found = [];
-    Object.keys(collection).forEach(function(key) {
-      var item = collection[key];
-      if(compare(item)) {
-        found.push(item);
-      }
-    });
-    return found;
-  }
-
-  // find the size of a collection (either array or object)
-  function size(collection) {
-    var length = 0;
-    Object.keys(collection).forEach(function(key) {
-      length = length + 1;
-    });
-    return length;
-  }
-
-  // Map an object's properties into an array
-  function objToArray(object) {
-    return Object.keys(object).map(function(key) {
-      return object[key];
-    });
-  }
-
-  // return the difference between two arrays
-  function diff(original, b) {
-    return original.filter(function(i) {return b.indexOf(i) < 0;});
-  }
-
-  // ensure type passed is a PerformanceResourceTiming object
-  function isResourceObject(resource) {
-    return resource instanceof PerformanceResourceTiming;
-  }
-
-  // ensure type passed is a PerformanceTiming object
-  function isTimingObject(timing) {
-    return timing instanceof PerformanceTiming;
-  }
-
-  // Citing - https://github.com/addyosmani/timing.js
-  function calcFirstPaintTime(window) {
-    var firstPaint = 0;
-    var firstPaintTime = null;
-    var performance = window.performance;
-    var chrome = window.chrome;
-
-    // Chrome
-    if (chrome && chrome.loadTimes) {
-      // Convert to ms
-      firstPaint = chrome.loadTimes().firstPaintTime * 1000;
-      firstPaintTime = firstPaint - (chrome.loadTimes().startLoadTime * 1000);
-    }
-    // IE
-    else if (typeof performance.timing.msFirstPaint === "number") {
-      firstPaint = performance.timing.msFirstPaint;
-      firstPaintTime = firstPaint - performance.timing.navigationStart;
-    }
-
-    return firstPaintTime;
-  }
-
-  // calculate navigation timing metrics
-  function calcTimingMetrics(timing) {
-
-    if (!isTimingObject(timing)) {
-      throw new Error("calcTimingMetrics(): arg bust be a PerformanceTiming object");
-    }
-
-    // Citing metrics from Addy Osmani and Perfbar
-    return Object.freeze({
-      // Total time from start to load
-      loadTime: timing.loadEventEnd - timing.navigationStart,
-      // Time spent constructing the DOM tree
-      domReadyTime: timing.domComplete - timing.domInteractive,
-      // Time consumed prepaing the new page
-      readyStart: timing.fetchStart - timing.navigationStart,
-      // Time spent during redirection
-      redirectTime: timing.redirectEnd - timing.redirectStart,
-      // AppCache
-      appcacheTime: timing.domainLookupStart - timing.fetchStart,
-      // Time spent unloading documents
-      unloadEventTime: timing.unloadEventEnd - timing.unloadEventStart,
-      // DNS query time
-      lookupDomainTime: timing.domainLookupEnd - timing.domainLookupStart,
-      // TCP connection time
-      connectTime: timing.connectEnd - timing.connectStart,
-      // Time spent during the request
-      requestTime: timing.responseEnd - timing.requestStart,
-      // Request to completion of the DOM loading
-      initDomTreeTime: timing.domInteractive - timing.responseEnd,
-      // Load event time
-      loadEventTime: timing.loadEventEnd - timing.loadEventStart,
-      // Time it takes to get the first byte from the server
-      latency: timing.responseStart - timing.connectStart,
-      // Time it takes to get the full response from the server
-      backend: timing.responseEnd - timing.navigationStart,
-      // Time it takes for the front end to load
-      frontend: timing.loadEventStart - timing.responseEnd,
-      // Time from DOM loading to the point where it can be used
-      domContentLoaded: timing.domContentLoadedEventStart - timing.domInteractive,
-      // Time it takes to process the page
-      processDuration: timing.loadEventStart - timing.domLoading,
-      // Time it takes to paint the first frame
-      firstPaintTime: calcFirstPaintTime(window)
-    });
-
-  }
-
-  // calculate metrics for individual resources
-  function calcResourceMetrics(resource) {
-
-    if (!isResourceObject(resource)) {
-      throw new Error("calcResourceMetrics(): arg bust be a PerformanceResourceTiming object");
-    }
-
-    return Object.freeze({
-      dns: resource.domainLookupEnd - resource.domainLookupStart,
-      tcp: resource.connectEnd - resource.connectStart,
-      timeToFirstByte: resource.responseStart - resource.startTime,
-      transfer: resource.responseEnd - resource.responseStart,
-      total: resource.responseEnd - resource.startTime
-    });
-  }
-
   function perf(params) {
     var performance = params.performance;
     var userAgent = params.userAgent;
@@ -146,6 +10,142 @@
     var timing = {};
     var timingMetrics = {};
     var uid = 0;
+
+    // create immutable listener object
+    function listener(params) {
+      return Object.freeze({
+        name: params.name,
+        event: params.event
+      });
+    }
+
+    // find all items in a collection that match a comparison's criteria
+    function findWhere(collection, compare) {
+      var found = [];
+      Object.keys(collection).forEach(function(key) {
+        var item = collection[key];
+        if(compare(item)) {
+          found.push(item);
+        }
+      });
+      return found;
+    }
+
+    // find the size of a collection (either array or object)
+    function size(collection) {
+      var length = 0;
+      Object.keys(collection).forEach(function(key) {
+        length = length + 1;
+      });
+      return length;
+    }
+
+    // Map an object's properties into an array
+    function objToArray(object) {
+      return Object.keys(object).map(function(key) {
+        return object[key];
+      });
+    }
+
+    // return the difference between two arrays
+    function diff(original, b) {
+      return original.filter(function(i) {return b.indexOf(i) < 0;});
+    }
+
+    // ensure type passed is a PerformanceResourceTiming object
+    function isResourceObject(resource) {
+      return resource instanceof PerformanceResourceTiming;
+    }
+
+    // ensure type passed is a PerformanceTiming object
+    function isTimingObject(timing) {
+      return timing instanceof PerformanceTiming;
+    }
+
+    // Citing - https://github.com/addyosmani/timing.js
+    function calcFirstPaintTime(window) {
+      var firstPaint = 0;
+      var firstPaintTime = null;
+      var performance = window.performance;
+      var chrome = window.chrome;
+
+      // Chrome
+      if (chrome && chrome.loadTimes) {
+        // Convert to ms
+        firstPaint = chrome.loadTimes().firstPaintTime * 1000;
+        firstPaintTime = firstPaint - (chrome.loadTimes().startLoadTime * 1000);
+      }
+      // IE
+      else if (typeof performance.timing.msFirstPaint === "number") {
+        firstPaint = performance.timing.msFirstPaint;
+        firstPaintTime = firstPaint - performance.timing.navigationStart;
+      }
+
+      return firstPaintTime;
+    }
+
+    // calculate navigation timing metrics
+    function calcTimingMetrics(timing) {
+
+      if (!isTimingObject(timing)) {
+        throw new Error("calcTimingMetrics(): arg bust be a PerformanceTiming object");
+      }
+
+      // Citing metrics from Addy Osmani and Perfbar
+      return Object.freeze({
+        // Total time from start to load
+        loadTime: timing.loadEventEnd - timing.navigationStart,
+        // Time spent constructing the DOM tree
+        domReadyTime: timing.domComplete - timing.domInteractive,
+        // Time consumed prepaing the new page
+        readyStart: timing.fetchStart - timing.navigationStart,
+        // Time spent during redirection
+        redirectTime: timing.redirectEnd - timing.redirectStart,
+        // AppCache
+        appcacheTime: timing.domainLookupStart - timing.fetchStart,
+        // Time spent unloading documents
+        unloadEventTime: timing.unloadEventEnd - timing.unloadEventStart,
+        // DNS query time
+        lookupDomainTime: timing.domainLookupEnd - timing.domainLookupStart,
+        // TCP connection time
+        connectTime: timing.connectEnd - timing.connectStart,
+        // Time spent during the request
+        requestTime: timing.responseEnd - timing.requestStart,
+        // Request to completion of the DOM loading
+        initDomTreeTime: timing.domInteractive - timing.responseEnd,
+        // Load event time
+        loadEventTime: timing.loadEventEnd - timing.loadEventStart,
+        // Time it takes to get the first byte from the server
+        latency: timing.responseStart - timing.connectStart,
+        // Time it takes to get the full response from the server
+        backend: timing.responseEnd - timing.navigationStart,
+        // Time it takes for the front end to load
+        frontend: timing.loadEventStart - timing.responseEnd,
+        // Time from DOM loading to the point where it can be used
+        domContentLoaded: timing.domContentLoadedEventStart - timing.domInteractive,
+        // Time it takes to process the page
+        processDuration: timing.loadEventStart - timing.domLoading,
+        // Time it takes to paint the first frame
+        firstPaintTime: calcFirstPaintTime(window)
+      });
+
+    }
+
+    // calculate metrics for individual resources
+    function calcResourceMetrics(resource) {
+
+      if (!isResourceObject(resource)) {
+        throw new Error("calcResourceMetrics(): arg bust be a PerformanceResourceTiming object");
+      }
+
+      return Object.freeze({
+        dns: resource.domainLookupEnd - resource.domainLookupStart,
+        tcp: resource.connectEnd - resource.connectStart,
+        timeToFirstByte: resource.responseStart - resource.startTime,
+        transfer: resource.responseEnd - resource.responseStart,
+        total: resource.responseEnd - resource.startTime
+      });
+    }
 
     // increment unique id
     var nextUid = function() {
